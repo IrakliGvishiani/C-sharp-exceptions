f5d6237852f31062bd52e68328be110859a6df0e
IrakliGvishiani <irakligvidhiani@gmail.com>
2026-02-04 22:03:34 +0400

    changed

--- ConsoleApp2/FootballTeams.cs
+++ ConsoleApp2/FootballTeams.cs
@@ -16,11 +16,29 @@ namespace ConsoleApp2
         public int titleWon { get; set; }
 
 
+        public FootballTeams Parse(string input)
+        {
+            string[] parts = input.Split('|');
+            FootballTeams team = new FootballTeams
+            {
+                clubID = int.Parse(parts[0].Trim()),
+                clubName = parts[1].Trim(),
+                country = parts[2].Trim(),
+                city = parts[3].Trim(),
+                foundedYear = int.Parse(parts[4].Trim()),
+                stadiumName = parts[5].Trim(),
+                titleWon = int.Parse(parts[6].Trim())
+            };
+            return team;
+        }
+
+        
         public void DisplayInfo()
         {
 
             string path = "C:\\Users\\user\\source\\repos\\ConsoleApp2\\ConsoleApp2\\Teams\\Team.txt";
 
+            
             string[] lines = System.IO.File.ReadAllLines(path);
 
 
@@ -32,49 +50,36 @@ namespace ConsoleApp2
             for (int i = 0; i < lines.Length; i++)
             {
 
-                string[] parts = lines[i].Split('|');
-
-                FootballTeams team = new()
-                {
-                    clubID = int.Parse(parts[0].Trim()),
-                    clubName = parts[1].Trim(),
-                    country = parts[2].Trim(),
-                    city = parts[3].Trim(),
-
-                    foundedYear = int.Parse(parts[4].Trim()),
-                    stadiumName = parts[5].Trim(),
-
-                    titleWon = int.Parse(parts[6].Trim())
+     
+                var team = Parse(lines[i]);
+                    teams[i] = team;
 
 
-                };
 
-                teams[i] = team;
-
-             //   Console.WriteLine(
-             //$@"Club ID: {team.clubID}
-             //   Club Name: {team.clubName}
-             //   Country: {team.country}
-             //   City: {team.city}
-             //   Founded Year: {team.foundedYear}
-             //   Stadium Name: {team.stadiumName}
-             //   Titles Won: {team.titleWon}
-             //   ----------------------------");
-            }
-
-
-            foreach (var team in teams)
-            {
                 Console.WriteLine(
-                  $@"Club ID: {team.clubID}
-                Club Name: {team.clubName}
-                Country: {team.country}
-                City: {team.city}
-                Founded Year: {team.foundedYear}
-                Stadium Name: {team.stadiumName}
-                Titles Won: {team.titleWon}
-                ----------------------------");
+             $@"Club ID: {team.clubID}
+                   Club Name: {team.clubName}
+                   Country: {team.country}
+                   City: {team.city}
+                   Founded Year: {team.foundedYear}
+                   Stadium Name: {team.stadiumName}
+                   Titles Won: {team.titleWon}
+                   ----------------------------");
             }
+
+
+            //foreach (var team in teams)
+            //{
+            //    Console.WriteLine(
+            //      $@"Club ID: {team.clubID}
+            //    Club Name: {team.clubName}
+            //    Country: {team.country}
+            //    City: {team.city}
+            //    Founded Year: {team.foundedYear}
+            //    Stadium Name: {team.stadiumName}
+            //    Titles Won: {team.titleWon}
+            //    ----------------------------");
+            //}
 
 
         }
